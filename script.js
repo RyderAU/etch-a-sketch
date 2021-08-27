113cfe491103afbf3ae238cead38ee1975bb1ce6
Ryder <ryder.jacka@gmail.com>
2021-08-28 02:11:48 +1000

    Grid rows and columns changed in script

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const container = document.querySelector('#container');
 
+container.style.gridTemplateColumns = `repeat(16, 20px)`;
+container.style.gridTemplateRows = `repeat(16, 20px)`;
+
 for (let i = 0 ; i < 256 ; i++) {
     const div = document.createElement('div');
     div.classList.add('square');
     container.appendChild(div);
-
 }
 
 const squares = document.querySelectorAll('.square');
