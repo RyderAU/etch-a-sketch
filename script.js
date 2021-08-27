68f3b586b9720c2ff4fbab4dfee531b9d4fd6e66
Ryder <ryder.jacka@gmail.com>
2021-08-28 03:26:21 +1000

    Clear grid button

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-const container = document.querySelector('#container');
+let gridSize = 16;
+const sizeSubmit = document.querySelector('#size-submit');
+sizeSubmit.addEventListener('click', changeSize);
 
-container.style.gridTemplateColumns = `repeat(16, 20px)`;
-container.style.gridTemplateRows = `repeat(16, 20px)`;
+const clearBtn = document.querySelector('#clear-button');
+clearBtn.addEventListener('click', clearGrid);
 
-for (let i = 0 ; i < 256 ; i++) {
-    const div = document.createElement('div');
-    div.classList.add('square');
-    container.appendChild(div);
+generateGrid(gridSize);
+
+function generateGrid(gridSize) {
+    const container = document.querySelector('#container');
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 20px)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 20px)`;
+
+    for (let i = 0 ; i < Math.pow(gridSize, 2) ; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+        container.appendChild(div);
+    }
+
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener('mouseover', colourSquare));
 }
 
-const squares = document.querySelectorAll('.square');
-
-squares.forEach(square => square.addEventListener('mouseover', colourSquare));
+function changeSize() {
+    gridSize = document.querySelector('#size-selection').value;
+    generateGrid(gridSize);
+}
 
 function colourSquare(e) {
     e.target.classList.add('coloured-square');
+}
+
+function clearGrid () {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.classList.remove('coloured-square'));
 }
